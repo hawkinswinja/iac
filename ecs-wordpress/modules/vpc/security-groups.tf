1879d15a1bdeb909f35f97fbdf7314cd27851e88
hawkinswinja <hawkinswinja@gmail.com>
2024-08-29 15:31:26 +0300

    feat: improved security group rules for better access control
    - Updated security-groups.tf to restrict access to private subnets
    - Removed outdated rules that allowed broader access
    
    This change improves the security posture by limiting access to critical resources.

--- ecs-wordpress/modules/vpc/security-groups.tf
+++ ecs-wordpress/modules/vpc/security-groups.tf
@@ -15,6 +15,7 @@ resource "aws_security_group" "public_sg" {
   }
 
   ingress {
+    description = "Allow inbound traffic from the internet on port 22"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -22,7 +23,7 @@ resource "aws_security_group" "public_sg" {
   }
 
   egress {
-    description = "Allow outbound traffic to private subnet"
+    description = "Allow outbound traffic to private subnets on port 80"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -30,80 +31,73 @@ resource "aws_security_group" "public_sg" {
   }
 
   egress {
+    description = "Allow outbound traffic to private subnets on port 22"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = var.private_subnet_cidr
   }
-}
-
-resource "aws_security_group" "private_sg" {
-  name        = "private_sg"
-  description = "Allow inbound traffic from public_sg on port 22 and 80"
-
-  vpc_id = aws_vpc.ecs-vpc.id
-
-  # ingress {
-  #   from_port       = 22
-  #   to_port         = 22
-  #   protocol        = "tcp"
-  #   security_groups = [aws_security_group.public_sg.id]
-  # }
-
-  # ingress {
-  #   from_port   = 3306
-  #   to_port     = 3306
-  #   protocol    = "tcp"
-  #   cidr_blocks = var.private_subnet_cidr
-  # }
-
-  # ingress {
-  #   from_port   = 80
-  #   to_port     = 80
-  #   protocol    = "tcp"
-  #   security_groups = [aws_security_group.public_sg.id]
-  # }
-
-  # egress {
-  #   from_port   = 3306
-  #   to_port     = 3306
-  #   protocol    = "tcp"
-  #   cidr_blocks = var.private_subnet_cidr
-  # }
-
-  # egress {
-  #   from_port   = 2049
-  #   to_port     = 2049
-  #   protocol    = "tcp"
-  #   security_groups = [ aws_security_group.efs_sg.id ]
-  # }
-
-  # egress {
-  #   from_port   = 1024
-  #   to_port     = 65535
-  #   protocol    = "tcp"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
-
-  ingress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    security_groups = [aws_security_group.public_sg.id, aws_security_group.efs_sg.id]
-  }
 
   egress {
+    description = "Allow outbound traffic to the internet on all ports"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+}
+
+resource "aws_security_group" "private_sg" {
+  name        = "private_sg"
+  description = "Allow inbound traffic from public_sg and efs_sg"
+
+  vpc_id = aws_vpc.ecs-vpc.id
+
+  ingress {
+    description = "Allow ssh from public security group"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    security_groups = [aws_security_group.public_sg.id]
+  }
+
+  ingress {
+    description = "Allow http from public security group"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    security_groups = [aws_security_group.public_sg.id]
+  }
+
+  egress {
+    description = "Allow mysql traffic to rds security group"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    security_groups = [aws_security_group.rds_sg.id]
+  }
+
+  egress {
+    description = "Allow https for connecting to ecs agent"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow nfs traffic to efs security group"
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    security_groups = [aws_security_group.efs_sg.id]
+  }
+ 
 }
 
 resource "aws_security_group" "rds_sg" {
   name        = "rds_sg"
-  description = "Allow mysql traffic from private_sg on port 3306"
+  description = "Allow mysql traffic from private subnets"
 
   vpc_id = aws_vpc.ecs-vpc.id
 
@@ -111,47 +105,21 @@ resource "aws_security_group" "rds_sg" {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
-    security_groups = [aws_security_group.private_sg.id]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    security_groups = [aws_security_group.private_sg.id]
+    cidr_blocks = var.private_subnet_cidr
   }
 }
 
 resource "aws_security_group" "efs_sg" {
   name        = "efs_sg"
-  description = "Allow inbound traffic from private_sg on port 2049"
+  description = "Allow nfs from private subnet on port 2049"
 
   vpc_id = aws_vpc.ecs-vpc.id
 
   ingress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = var.private_subnet_cidr
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port       = 2049
+    to_port         = 2049
+    protocol        = "tcp"
     cidr_blocks = var.private_subnet_cidr
   }
 
-  # ingress {
-  #   from_port       = 2049
-  #   to_port         = 2049
-  #   protocol        = "nfs"
-  #   security_groups = [aws_security_group.private_sg.id]
-  # }
-
-  # egress = {
-  #   from_port   = 1024
-  #   to_port     = 65535
-  #   protocol    = "tcp"
-  #   security_groups = [ aws_security_group.private_sg.id ]
 }
